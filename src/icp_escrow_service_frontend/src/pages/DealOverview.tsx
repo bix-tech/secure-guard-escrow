dfa48648225585f9047ad5a57afdd584eb522735
Yong zhi <yongzhi@bixtech.co>
2023-12-27 17:46:53 +0800

    Creating a template for DealOverview.

--- src/icp_escrow_service_frontend/src/pages/DealOverview.tsx
+++ src/icp_escrow_service_frontend/src/pages/DealOverview.tsx
@@ -5,11 +5,21 @@ const DealOverview = () => {
             <div>
                 Deal Overview
             </div>
-            <div className="card p-5 mx-auto my-5" style={{ width: '50%' }}>
+            <div className="d-flex flex-row justify-content-between p-5">
+                <div className="card p-5 mx-auto my-5" style={{ width: '45%' }}>
 
+                </div>
+                <div className="card p-5 mx-auto my-5" style={{ width: '45%' }}>
+                    Deal Description
+                </div>
             </div>
-            <div className="card p-5 mx-auto my-5" style={{ width: '50%' }}>
 
+            <div className="card p-5 mx-auto my-5" style={{ width: '80%' }}>
+                Deliverables
+            </div>
+
+            <div className="card p-5 mx-auto my-5" style={{ width: '80%' }}>
+                Activity Logs
             </div>
 
         </div>
