ff580378897af4d6e3d84ab5f5eccdf9bedaa5ba
Yong zhi <yongzhi@bixtech.co>
2023-12-27 13:46:55 +0800

    Added script for npm run setup. Changing image location to public.

--- src/icp_escrow_service_frontend/src/components/CreateDealProgressBar.tsx
+++ src/icp_escrow_service_frontend/src/components/CreateDealProgressBar.tsx
@@ -11,7 +11,7 @@ const CreateDealProgressBar: React.FC<CreateDealProgressBarProps> = ({ currentSt
             <div className="d-flex justify-content-between align-items-center">
                 <div className={`state-bar d-flex flex-column align-items-center ${currentStep === 1 ? 'active' : ''}`} style={{ width: '20%' }}>
                     <div className="avatar state-avatar">
-                        <img src="src/assets/images/create_deal_icon.png" alt="State Avatar 1" />
+                        <img src="/create_deal_icon.png" alt="State Avatar 1" />
                     </div>
                     <div className="mt-2">Create Deal</div>
                 </div>
@@ -20,7 +20,7 @@ const CreateDealProgressBar: React.FC<CreateDealProgressBarProps> = ({ currentSt
 
                 <div className={`state-bar d-flex flex-column align-items-center ${currentStep === 2 ? 'active' : ''}`} style={{ width: '20%' }}>
                     <div className="avatar state-avatar">
-                    <img src={currentStep >= 2 ? "src/assets/images/deal-details-icon.png" : "src/assets/images/deal-details-icon-2.png"} alt="State Avatar 2" />
+                    <img src={currentStep >= 2 ? "/deal-details-icon.png" : "/deal-details-icon-2.png"} alt="State Avatar 2" />
                     </div>
                     <div className="mt-2">Deal Details</div>
                 </div>
@@ -29,7 +29,7 @@ const CreateDealProgressBar: React.FC<CreateDealProgressBarProps> = ({ currentSt
 
                 <div className={`state-bar d-flex flex-column align-items-center ${currentStep >= 3 ? 'active' : ''}`} style={{ width: '20%' }}>
                     <div className="avatar state-avatar">
-                        <img src={currentStep >= 3 ? "src/assets/images/verify.png" : "src/assets/images/verify-2.png"} alt="State Avatar 3" />
+                        <img src={currentStep >= 3 ? "/verify.png" : "/verify-2.png"} alt="State Avatar 3" />
                     </div>
                     <div className="mt-2">Deal Submitted</div>
                 </div>
